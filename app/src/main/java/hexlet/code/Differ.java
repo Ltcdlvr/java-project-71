5abe9d1ffb090cd405cf4f0dc2866dd739034b63
mk723 <120035203+mk723@users.noreply.github.com>
2023-08-21 23:36:30 +0200

    update java+gradle versions

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,50 +2,49 @@ package hexlet.code;
 
 import hexlet.code.formatters.FormatterInterface;
 
-//import java.util.HashSet;
-//import java.util.Map;
-//import java.util.Set;
-//import java.util.SortedMap;
-//import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 
 public class Differ {
 
     public static String generate(String firstURI, String secondURI, FormatterInterface formatter) throws Exception {
-//        Map<String, Object> firstObject = Parser.parseFile(firstURI);
-//        Map<String, Object> secondObject = Parser.parseFile(secondURI);
-//        Map<String, CompositeValue> actualDiff = Differ.findDiff(firstObject, secondObject);
-//        return formatter.getString(actualDiff);
-        return " ";
+        Map<String, Object> firstObject = Parser.parseFile(firstURI);
+        Map<String, Object> secondObject = Parser.parseFile(secondURI);
+        Map<String, CompositeValue> actualDiff = Differ.findDiff(firstObject, secondObject);
+        return formatter.getString(actualDiff);
     }
 
-//    public static Map<String, CompositeValue> findDiff(Map<String, Object> firstMap, Map<String, Object> secondMap) {
-//        Set<String> firstKeys = new HashSet<>(firstMap.keySet());
-//        Set<String> secondKeys = new HashSet<>(secondMap.keySet());
-//
-//        Set<String> intersectionKeys = new HashSet<>(firstKeys);
-//        intersectionKeys.retainAll(secondKeys);
-//
-//        // result
-//        SortedMap<String, CompositeValue> sortedResult = new TreeMap<>();
-//
-//        // deleted keys
-//        firstKeys.removeAll(intersectionKeys);
-//        for (String deletedKey: firstKeys) {
-//            sortedResult.put(deletedKey, new CompositeValue(String.valueOf(firstMap.get(deletedKey)), null));
-//        }
-//
-//        // added keys
-//        secondKeys.removeAll(intersectionKeys);
-//        for (String addedKey: secondKeys) {
-//            sortedResult.put(addedKey, new CompositeValue(null, String.valueOf(secondMap.get(addedKey))));
-//        }
-//
-//        // keys in intersection
-//        for (String key: intersectionKeys) {
-//            sortedResult.put(key, new CompositeValue(String.valueOf(firstMap.get(key)),
-//                    String.valueOf(secondMap.get(key))));
-//        }
-//        return sortedResult;
-//    }
+    public static Map<String, CompositeValue> findDiff(Map<String, Object> firstMap, Map<String, Object> secondMap) {
+        Set<String> firstKeys = new HashSet<>(firstMap.keySet());
+        Set<String> secondKeys = new HashSet<>(secondMap.keySet());
+
+        Set<String> intersectionKeys = new HashSet<>(firstKeys);
+        intersectionKeys.retainAll(secondKeys);
+
+        // result
+        SortedMap<String, CompositeValue> sortedResult = new TreeMap<>();
+
+        // deleted keys
+        firstKeys.removeAll(intersectionKeys);
+        for (String deletedKey: firstKeys) {
+            sortedResult.put(deletedKey, new CompositeValue(String.valueOf(firstMap.get(deletedKey)), null));
+        }
+
+        // added keys
+        secondKeys.removeAll(intersectionKeys);
+        for (String addedKey: secondKeys) {
+            sortedResult.put(addedKey, new CompositeValue(null, String.valueOf(secondMap.get(addedKey))));
+        }
+
+        // keys in intersection
+        for (String key: intersectionKeys) {
+            sortedResult.put(key, new CompositeValue(String.valueOf(firstMap.get(key)),
+                    String.valueOf(secondMap.get(key))));
+        }
+        return sortedResult;
+    }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,95 +1,95 @@
 package hexlet.code;
 
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import java.util.HashMap;
-//import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DifferTest {
-//    @Test
-//    @DisplayName("test Differ Json")
-//    void testDifferJson() throws Exception {
-//
-//        Map<String, Object> firstObject = Parser.parseFile("./src/test/resources/1.json");
-//        Map<String, Object> secondObject = Parser.parseFile("./src/test/resources/2.json");
-//        Map<String, CompositeValue> diff = Differ.findDiff(firstObject, secondObject);
-//
-//        Map<String, CompositeValue> expected = new HashMap<>(
-//                Map.of("follow", new CompositeValue("false", null),
-//                "host", new CompositeValue("hexlet.io", "hexlet.io"),
-//                "proxy", new CompositeValue("123.234.53.22", null),
-//                "timeout", new CompositeValue("50", "20"),
-//                "verbose", new CompositeValue(null, "true")));
-//        assertTrue(diff.equals(expected));
-//    }
-//
-//    @Test
-//    @DisplayName("test Differ Yaml")
-//    void testDifferYaml() throws Exception {
-//
-//        Map<String, Object> firstObject = Parser.parseFile("./src/test/resources/1.yaml");
-//        Map<String, Object> secondObject = Parser.parseFile("./src/test/resources/2.yaml");
-//        Map<String, CompositeValue> diff = Differ.findDiff(firstObject, secondObject);
-//
-//        Map<String, CompositeValue> expected = new HashMap<>(
-//                Map.of("follow", new CompositeValue("false", null),
-//                "host", new CompositeValue("hexlet.io", "hexlet.io"),
-//                "proxy", new CompositeValue("123.234.53.22", null),
-//                "timeout", new CompositeValue("50", "20"),
-//                "verbose", new CompositeValue(null, "true")));
-//        assertTrue(diff.equals(expected));
-//    }
-//
-//    @Test
-//    @DisplayName("test Differ Stylish")
-//    void testDifferStylish() throws Exception {
-//
-//        String diff = Differ.generate("./src/test/resources/1.json",
-//                "./src/test/resources/2.json", Formatter.getFormatter("stylish"));
-//
-//        String expected = "{\n"
-//                + "\t- follow: false\n"
-//                + "\t  host: hexlet.io\n"
-//                + "\t- proxy: 123.234.53.22\n"
-//                + "\t- timeout: 50\n"
-//                + "\t+ timeout: 20\n"
-//                + "\t+ verbose: true\n"
-//                + "}";
-//
-//        assertTrue(diff.equals(expected));
-//    }
-//
-//    @Test
-//    @DisplayName("test Differ Plain")
-//    void testDifferPlain() throws Exception {
-//
-//        String diff = Differ.generate("./src/test/resources/1.json",
-//                "./src/test/resources/2.json", Formatter.getFormatter("plain"));
-//
-//        String expected = "Property 'follow' was removed\n"
-//                + "Property 'host' was unchanged with value: 'hexlet.io'\n"
-//                + "Property 'proxy' was removed\n"
-//                + "Property 'timeout' was updated. From '50' to '20'\n"
-//                + "Property 'verbose' was added with value: 'true'";
-//
-//        assertTrue(diff.equals(expected));
-//    }
-//
-//    @Test
-//    @DisplayName("test Differ Json format")
-//    void testDifferJsonFormat() throws Exception {
-//
-//        String diff = Differ.generate("./src/test/resources/1.json",
-//                "./src/test/resources/2.json", Formatter.getFormatter("json"));
-//
-//        String expected = "{\"follow\":{\"oldValue\":\"false\",\"newValue\":null},\"host\":{\"oldValue\":"
-//                + "\"hexlet.io\",\"newValue\":\"hexlet.io\"},\"proxy\":{\"oldValue\":\"123.234.53.22\","
-//                + "\"newValue\":null},\"timeout\":{\"oldValue\":\"50\",\"newValue\":\"20\"},\"verbose\""
-//                + ":{\"oldValue\":null,\"newValue\":\"true\"}}";
-//
-//        assertTrue(diff.equals(expected));
-//    }
+    @Test
+    @DisplayName("test Differ Json")
+    void testDifferJson() throws Exception {
+
+        Map<String, Object> firstObject = Parser.parseFile("./src/test/resources/1.json");
+        Map<String, Object> secondObject = Parser.parseFile("./src/test/resources/2.json");
+        Map<String, CompositeValue> diff = Differ.findDiff(firstObject, secondObject);
+
+        Map<String, CompositeValue> expected = new HashMap<>(
+                Map.of("follow", new CompositeValue("false", null),
+                "host", new CompositeValue("hexlet.io", "hexlet.io"),
+                "proxy", new CompositeValue("123.234.53.22", null),
+                "timeout", new CompositeValue("50", "20"),
+                "verbose", new CompositeValue(null, "true")));
+        assertTrue(diff.equals(expected));
+    }
+
+    @Test
+    @DisplayName("test Differ Yaml")
+    void testDifferYaml() throws Exception {
+
+        Map<String, Object> firstObject = Parser.parseFile("./src/test/resources/1.yaml");
+        Map<String, Object> secondObject = Parser.parseFile("./src/test/resources/2.yaml");
+        Map<String, CompositeValue> diff = Differ.findDiff(firstObject, secondObject);
+
+        Map<String, CompositeValue> expected = new HashMap<>(
+                Map.of("follow", new CompositeValue("false", null),
+                "host", new CompositeValue("hexlet.io", "hexlet.io"),
+                "proxy", new CompositeValue("123.234.53.22", null),
+                "timeout", new CompositeValue("50", "20"),
+                "verbose", new CompositeValue(null, "true")));
+        assertTrue(diff.equals(expected));
+    }
+
+    @Test
+    @DisplayName("test Differ Stylish")
+    void testDifferStylish() throws Exception {
+
+        String diff = Differ.generate("./src/test/resources/1.json",
+                "./src/test/resources/2.json", Formatter.getFormatter("stylish"));
+
+        String expected = "{\n"
+                + "\t- follow: false\n"
+                + "\t  host: hexlet.io\n"
+                + "\t- proxy: 123.234.53.22\n"
+                + "\t- timeout: 50\n"
+                + "\t+ timeout: 20\n"
+                + "\t+ verbose: true\n"
+                + "}";
+
+        assertTrue(diff.equals(expected));
+    }
+
+    @Test
+    @DisplayName("test Differ Plain")
+    void testDifferPlain() throws Exception {
+
+        String diff = Differ.generate("./src/test/resources/1.json",
+                "./src/test/resources/2.json", Formatter.getFormatter("plain"));
+
+        String expected = "Property 'follow' was removed\n"
+                + "Property 'host' was unchanged with value: 'hexlet.io'\n"
+                + "Property 'proxy' was removed\n"
+                + "Property 'timeout' was updated. From '50' to '20'\n"
+                + "Property 'verbose' was added with value: 'true'";
+
+        assertTrue(diff.equals(expected));
+    }
+
+    @Test
+    @DisplayName("test Differ Json format")
+    void testDifferJsonFormat() throws Exception {
+
+        String diff = Differ.generate("./src/test/resources/1.json",
+                "./src/test/resources/2.json", Formatter.getFormatter("json"));
+
+        String expected = "{\"follow\":{\"oldValue\":\"false\",\"newValue\":null},\"host\":{\"oldValue\":"
+                + "\"hexlet.io\",\"newValue\":\"hexlet.io\"},\"proxy\":{\"oldValue\":\"123.234.53.22\","
+                + "\"newValue\":null},\"timeout\":{\"oldValue\":\"50\",\"newValue\":\"20\"},\"verbose\""
+                + ":{\"oldValue\":null,\"newValue\":\"true\"}}";
+
+        assertTrue(diff.equals(expected));
+    }
 }
 
