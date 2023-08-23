467bb8d645d81a493b9fb5dfe1ce6104d352e86a
mk723 <120035203+mk723@users.noreply.github.com>
2023-08-24 00:38:13 +0200

    bugfix 3

--- app/src/main/java/hexlet/code/DifSearcher.java
+++ app/src/main/java/hexlet/code/DifSearcher.java
@@ -1,10 +1,11 @@
 package hexlet.code;
 
-import java.util.TreeSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class DifSearcher {
     public static List<CompositeValue> findDiff(Map<String, Object> firstMap, Map<String, Object> secondMap) {
@@ -23,8 +24,14 @@ public class DifSearcher {
                 type = "added";
             } else if (!secondMap.containsKey(key)) {
                 type = "deleted";
-            } else if (firstObject.equals(secondObject)) {
-                type = "unchanged";
+            } else {
+                if (Objects.isNull(firstObject)) {
+                    if (Objects.isNull(secondObject)) {
+                        type = "unchanged";
+                    }
+                } else if (firstObject.equals(secondObject)) {
+                    type = "unchanged";
+                }
             }
 
             sortedResult.add(new CompositeValue(key, type, firstObject, secondObject));
